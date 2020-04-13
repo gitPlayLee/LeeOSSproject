4f76a5eeb3b30eea01379aafceaa24a25e4a3a5c
gitPlayLee <lion7542@naver.com>
2020-04-13 13:46:27 +0900

    add fc Sub

--- hello.c
+++ hello.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
+void subfc (){
+	int a, b;
+	int sum = 0;
+	a = 10;
+	b = 10;
+	sum = a +b;
+	printf("sum = %d", &sum);
+}
 int main (){
 	printf("안녕하세요. hello world!!!!\n");
 	return 0;
